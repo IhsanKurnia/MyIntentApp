8f10d9b207dc0b5d0479127b5345260e4daeeaa7
IhsanKurnia22 <kurniaihsan58@gmail.com>
2020-09-15 23:40:31 +0700

    Tambah activity_move_with_data.xml

--- app/src/main/java/com/ihsan_kurnia/myintentapp/MainActivity.java
+++ app/src/main/java/com/ihsan_kurnia/myintentapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ihsan_kurnia.myintentapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,12 +16,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Inistialisasi
+        //Inistialisasi btnActivity
         Button btnMoveActivity = findViewById(R.id.btn_move_activity);
         btnMoveActivity.setOnClickListener(this);
 
+        //Inisialisais btnWithData
         Button btnMoveWithDataActivity = findViewById(R.id.btn_move_activity_data);
         btnMoveWithDataActivity.setOnClickListener(this);
+
+        //Inisialisasi btnDialNumber
+        Button btnDialPhone = findViewById(R.id.btn_dial_number);
+        btnDialPhone.setOnClickListener(this);
+
+        //Inisialisasi btnCamera
+        Button btnCamera = findViewById(R.id.btn_camera);
+        btnCamera.setOnClickListener(this);
     }
 
     @Override
@@ -35,6 +46,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 22);
                 startActivity(moveWithDataIntent);
                 break;
+            case R.id.btn_dial_number:
+                String phoneNumber = "082115844233";
+                Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+                startActivity(dialPhoneIntent);
+                break;
+            case R.id.btn_camera:
+                startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE_SECURE), 0);
         }
     }
 }
